50042fd60d1d6f369617e4b804c6623e173f8880
nsomazr <zreuben815@gmail.com>
2024-05-17 21:49:15 +0300

    read news

--- ai4ch/urls.py
+++ ai4ch/urls.py
@@ -1,6 +1,7 @@
-"""ai4ch URL Configuration
+"""
+ai4ch URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information, please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
@@ -13,44 +14,41 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include, re_path
-from ai4chapp import urls as a4chapp_urls
-from users import urls as users_urls
-from news import urls as news_urls
-from django.contrib.auth import views as auth_views 
-from beans import urls as beans_urls
-from cassava import urls as cassava_urls
-from maize import urls as maize_urls
-from rice import urls as rice_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from django.contrib.auth import views as auth_views
 from ckeditor_uploader import views as ckeditor_views
 
-# from users.forms import ConfirmResetForms
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    # include django browserable 
-    path("auth-api/", include("rest_framework.urls", namespace="rest_framework")),
-    path('',include(a4chapp_urls)),
-    path('users/', include(users_urls)),
-    path('news/', include(news_urls)),
-    # we can mention them this way
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password/password_reset_done.html'), name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(form_class=ConfirmResetForm, template_name="users/password/password_reset_confirm.html"), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password/password_reset_complete.html'), name='password_reset_complete'),   
-    # for social media authentication
-    path('accounts/', include('allauth.urls')),   
+    path('admin/', admin.site.urls),
+    # Include Django browsable API
+    path('auth-api/', include('rest_framework.urls', namespace='rest_framework')),
+    
+    # Application URLs
+    path('', include('ai4chapp.urls')),
+    path('users/', include('users.urls')),
+    path('news/', include('news.urls', namespace='news')),
+    path('beans/', include('beans.urls')),
+    path('cassava/', include('cassava.urls')),
+    path('maize/', include('maize.urls')),
+    path('rice/', include('rice.urls')),
+    
+    # Password reset views
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/password/password_reset_done.html'), name='password_reset_done'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password/password_reset_complete.html'), name='password_reset_complete'),
+    
+    # Social authentication
+    path('accounts/', include('allauth.urls')),
+    
+    # WYSIWYG editor
     path('tinymce/', include('tinymce.urls')),
-    path('beans/', include(beans_urls)),
-    path('cassava/', include(cassava_urls)),
-    path('maize/', include(maize_urls)),
-    path('rice/', include(rice_urls)),
-    # path('ckeditor/upload/', login_required(ckeditor_views.upload), name='ckeditor_upload'),
-    # path('ckeditor/browse/', never_cache(login_required(ckeditor_views.browse)), name='ckeditor_browse'),
     re_path(r'^ckeditor/upload/', login_required(ckeditor_views.upload), name='ckeditor_upload'),
     re_path(r'^ckeditor/browse/', never_cache(login_required(ckeditor_views.browse)), name='ckeditor_browse'),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- news/urls.py
+++ news/urls.py
@@ -1,20 +1,19 @@
-from django.urls import path,re_path
+from django.urls import path
 from .views import NewsAPIView
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = "news"  
+app_name = "news"
 
 urlpatterns = [
-               path("news/",NewsAPIView.news, name='news' ),
-               path('add-new/', NewsAPIView.add_new, name="add-new"),
-               path('new/<str:slug>/', NewsAPIView.read_blog, name="read-new"),
-               path('news-list/review-new/publish-new/<int:id>', NewsAPIView.publish_new, name="publish-new"),
-               path('news-list/review-new/<int:id>', NewsAPIView.review_new, name="review-new"),
-               path('news-list/view-new/<int:id>', NewsAPIView.view_new, name="view-new"),
-               path('news-list/delete-new/<int:id>', NewsAPIView.delete_new, name="delete-new"),
-               path('news-list/', NewsAPIView.news_list, name="news-list"),
-               path('api/news', NewsAPIView.as_view(), name="news-api")] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("", NewsAPIView.news, name='news'),
+    path('add-new/', NewsAPIView.add_new, name="add-new"),
+    path('<str:slug>/', NewsAPIView.read_blog, name="read-new"),
+    path('news/<str:slug>/', NewsAPIView.read_blog, name="read-new"),
+    path('news-list/review-new/publish-new/&lt;int:id&gt;', NewsAPIView.publish_new, name="publish-new"),
+    path('news-list/review-new/&lt;int:id&gt;', NewsAPIView.review_new, name="review-new"),
+    path('news-list/view-new/&lt;int:id&gt;', NewsAPIView.view_new, name="view-new"),
+    path('news-list/delete-new/&lt;int:id&gt;', NewsAPIView.delete_new, name="delete-new"),
+    path('news-list/', NewsAPIView.news_list, name="news-list"),
+    path('api/news', NewsAPIView.as_view(), name="news-api")
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
