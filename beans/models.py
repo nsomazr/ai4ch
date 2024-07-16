59bfcf1d5d037d28c662bdf12181f931a403beb5
nsomazr <zreuben815@gmail.com>
2024-07-16 10:51:55 +0300

    change dbs

--- beans/models.py
+++ beans/models.py
@@ -6,7 +6,6 @@ from django.utils import timezone
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.relpath(__file__)))
 
-
 class BeansData(models.Model):
     file_id = models.CharField(max_length=100)
     file_path = models.FileField(max_length=200,upload_to=os.path.join(BASE_DIR,'files'))
