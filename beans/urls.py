7bd337a9a120598b8fc382a097f3c62b2c4bcc86
nsomazr <zreuben815@gmail.com>
2025-02-18 18:09:11 +0300

    update endpond

--- beans/urls.py
+++ beans/urls.py
@@ -7,7 +7,7 @@ app_name = "beans"
 
 urlpatterns = [
     path('predict/', view=views.PredictImageView.as_view(), name='predict'),
-    path('detect/', view=views.BeansDetectAPI.as_view(), name='detect'),
+    # path('detect/', view=views.BeansDetectAPI.as_view(), name='detect'),
     path('image-beans-desease-classifier',view=views.image_beans_classifier, name='image-beans-classifier'),
     path('image-beans-desease-detector',view=views.image_beans_detect, name='image-beans-detector'),
     path('video-beans-desease-detector',view=views.video_beans_detect, name='video-beans-detector'),
--- maize/urls.py
+++ maize/urls.py
@@ -8,7 +8,7 @@ app_name = "maize"
 
 urlpatterns = [
     path('predict/', view=views.MaizePredictImageView.as_view(), name='predict'),
-    path('detect/', view=views.MaizeDetectAPI.as_view(), name='detect'),
+    # path('detect/', view=views.MaizeDetectAPI.as_view(), name='detect'),
     path('image-maize-desease-classifier',view=views.image_maize_classifier, name='image-maize-classifier'),
     path('image-maize-desease-detector',view=views.image_maize_detect, name='image-maize-detector'),
     path('video-maize-desease-detector',view=views.video_maize_detect, name='video-maize-detector'),
