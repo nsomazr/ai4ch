6fe3aa9a00b555d00363ac01b58e4cd33de69871
nsomazr <zreuben815@gmail.com>
2025-02-18 17:48:38 +0300

    api endpont

--- cassava/urls.py
+++ cassava/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 app_name = "cassava"  
 
 urlpatterns = [
-   path('predict/', view=views.CassavaPredictImageView.as_view(), name='predict'),
-    path('detect/', view=views.CassavaDetectImageAPI.as_view(), name='detect'),
+#    path('predict/', view=views.CassavaPredictImageView.as_view(), name='predict'),
+#     path('detect/', view=views.CassavaDetectImageAPI.as_view(), name='detect'),
     path('image-cassava-desease-classifier',view=views.image_cassava_classifier, name='image-cassava-classifier'),
     path('image-cassava-desease-detector',view=views.image_cassava_detect, name='image-cassava-detector'),
     path('video-cassava-desease-detector',view=views.video_cassava_detect, name='video-cassava-detector'),
